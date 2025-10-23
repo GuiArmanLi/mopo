f1213092324c3b39175632de9ae1629ea7312fb2
Guilherme Santana Pessa <103978528+GuiArmanLi@users.noreply.github.com>
2025-10-23 20:45:56 -0300

    refactor: separa card de produtos para componente próprio.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 import { HeaderNavigationComponent } from "../../shared/header-navigation/header-navigation.component";
 import { FooterNavigationComponent } from "../../shared/footer-navigation/footer-navigation.component";
+import { CardProdutoComponent } from '../../shared/card-produto/card-produto.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [HeaderNavigationComponent, FooterNavigationComponent],
+  imports: [HeaderNavigationComponent, CardProdutoComponent, FooterNavigationComponent],
   standalone: true,
 })
 export class HomeComponent {
---
+++ src/app/shared/card-produto/card-produto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-card-produto',
+  templateUrl: './card-produto.component.html',
+  styleUrl: './card-produto.component.scss',
+  imports: [RouterLink],
+})
+export class CardProdutoComponent {
+
+}
