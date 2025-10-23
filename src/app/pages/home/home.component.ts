12702f994904b74ce0299e37951526ee25b840e5
Guilherme Santana Pessa <103978528+GuiArmanLi@users.noreply.github.com>
2025-10-23 18:11:35 -0300

    refactor: separa links de navegação para componente próprio.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+
+import { HeaderNavigationComponent } from "../../shared/header-navigation/header-navigation.component";
 import { FooterNavigationComponent } from "../../shared/footer-navigation/footer-navigation.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [FooterNavigationComponent],
+  imports: [HeaderNavigationComponent, FooterNavigationComponent],
   standalone: true,
 })
 export class HomeComponent {
--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterNavigationComponent } from '../../shared/footer-navigation/footer-navigation.component';
+import { HeaderNavigationComponent } from "../../shared/header-navigation/header-navigation.component";
 
 @Component({
   selector: 'app-store',
-  imports: [FooterNavigationComponent],
+  imports: [FooterNavigationComponent, HeaderNavigationComponent],
   templateUrl: './store.component.html',
   styleUrl: './store.component.scss'
 })
