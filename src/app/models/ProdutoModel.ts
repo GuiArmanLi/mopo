bc9724b094850b6b3902dd31d729df0a77d7f5a3
Guilherme Santana Pessa <103978528+GuiArmanLi@users.noreply.github.com>
2025-10-26 07:54:15 -0300

    feat: adiciona o consumo de dados via API
    API simulada com o arquivo data.json. Configuração serviços HTTP com função provideHttpClient()

---
+++ src/app/models/ProdutoModel.ts
@@ -0,0 +1,9 @@
+export class ProdutoModel {
+  id!: number;
+  nome!: string;
+  tipo!: string;
+  descricao!: string;
+  dataDeValidade!: Date;
+  preco!: number;
+  imagem!: string;
+}
--- src/app/shared/card-produto/card-produto.component.ts
+++ src/app/shared/card-produto/card-produto.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ProdutoModel } from '../../models/ProdutoModel';
 
 @Component({
   selector: 'app-card-produto',
   templateUrl: './card-produto.component.html',
   styleUrl: './card-produto.component.scss',
   imports: [RouterLink],
+  standalone: true
 })
-export class CardProdutoComponent {
+export class CardProdutoComponent implements OnInit {
+  @Input() produto!: ProdutoModel;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if (this.produto) {
+      console.log('Objeto recebido no filho:', this.produto.nome);
+    }
+  }
 
 }
